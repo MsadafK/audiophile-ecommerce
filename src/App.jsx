56c89fcd077ab2bd971fc736491427d39db09395
MsadafK <mssadaf0@gmail.com>
2026-04-21 13:45:29 +0530

    basic structure ready for UI development

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Category from './pages/Category'
-import Product from './pages/Product'
-import Checkout from './pages/Checkout'
+import { Routes, Route } from "react-router-dom";
+
+// 🔁 Shared Components
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+// 📄 Pages
+import Home from "./pages/Home/Home";
+import Category from "./pages/Category";
+import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/category/:category" element={<Category />} />
-      <Route path="/product/:slug" element={<Product />} />
-      <Route path="/checkout" element={<Checkout />} />
-    </Routes>
-  )
+    <div className="bg-light-gray min-h-screen flex flex-col">
+      
+      {/* 🔝 Global Navbar */}
+      <Navbar />
+
+      {/* 📄 Routes */}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:category" element={<Category />} />
+          <Route path="/product/:slug" element={<Product />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </main>
+
+      {/* 🔻 Global Footer */}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import {
+  HeroSection,
+  CategorySection,
+  ZX9Section,
+  ZX7Section,
+  YX1Section,
+  BestGearSection,
+} from "./components";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <CategorySection />
+      <ZX9Section />
+      <ZX7Section />
+      <YX1Section />
+      <BestGearSection />
+    </>
+  );
+}
